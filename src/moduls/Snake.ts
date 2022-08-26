8dd15ff1e8394a0d9125598a697c45018903af44
haungxi <1842196429@qq.com>
2022-08-26 10:02:45 +0800

    完成贪吃蛇开发

--- src/moduls/Snake.ts
+++ src/moduls/Snake.ts
@@ -22,22 +22,64 @@ class Snake {
     set x(value: number) {
         if (this.x === value)
             return;
-        if (value < 0 || value > 200) {
+        if (value < 0 || value > 290) {
             throw new Error('蛇撞墙了')
         }
-        this.head.style.left = value + 'px'
+        if (this.bodies[1] && (this.bodies[1] as HTMLElement).offsetLeft === value) {
+            if (value > this.x) {
+                value = this.x - 10
+            } else {
+                value = this.x + 10
+            }
+        }
+
+        this.moveBody();
+        this.head.style.left = value + 'px';
+        this.checkheadBody()
+
     }
     set y(value: number) {
         if (this.y === value)
             return
-        if (value < 0 || value > 200) {
+        if (value < 0 || value > 290) {
             throw new Error('蛇撞墙了')
         }
-        this.head.style.top = value + 'px'
+        if (this.bodies[1] && (this.bodies[1] as HTMLElement).offsetTop === value) {
+            if (value > this.y) {
+                value = this.y - 10
+            } else {
+                value = this.y + 10
+            }
+        }
+
+        this.moveBody();
+        this.head.style.top = value + 'px';
+        this.checkheadBody()
     }
     // 增加蛇身长度
     addBody() {
         this.element.insertAdjacentHTML('beforeend', '<div></div>')
+    }
+    // 添加蛇身移动方法
+    moveBody() {
+        //将后面的值移动到前面的一节
+        for (let i = this.bodies.length - 1; i > 0; i--) {
+            let x = (this.bodies[i - 1] as HTMLElement).offsetLeft
+            let y = (this.bodies[i - 1] as HTMLElement).offsetTop;
+            (this.bodies[i] as HTMLElement).style.left = x + 'px';
+            (this.bodies[i] as HTMLElement).style.top = y + 'px';
+        }
+    }
+
+
+    checkheadBody() {
+        // 检查所有的身体,检查是否蛇头和蛇身的坐标相同
+        for (let i = 1; i < this.bodies.length; i++) {
+            let bd = this.bodies[i] as HTMLElement
+            if (this.x === bd.offsetLeft && this.y === bd.offsetTop) {
+                throw new Error("撞到自己了~~~")
+            }
+        }
     }
 
 }
