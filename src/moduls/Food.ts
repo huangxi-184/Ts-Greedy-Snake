ee34fde6a5bb272424f8b683bb2a57d77458b574
haungxi <1842196429@qq.com>
2022-08-26 10:19:14 +0800

    完成贪吃蛇的调试

--- src/moduls/Food.ts
+++ src/moduls/Food.ts
@@ -1,5 +1,6 @@
 // 定义食物类Food 
 class Food {
+    //获取HTML节点 食物div
     element: HTMLElement;
     constructor() {
         // 获取页面中的food的元素 并将值赋给element
@@ -19,8 +20,10 @@ class Food {
     change() {
         // 生成随机的位置 需要知道坐标的范围 
         // x最小 0 最大是290 为了保证位置的判断 所以食物位置必须10的整数倍
-        let left = Math.round(Math.random() * 29) * 10
-        let top = Math.round(Math.random() * 29) * 10
+        // 去除食物刷新沿边墙问题
+        let left = (Math.round(Math.random() * 27) + 1) * 10
+        let top = (Math.round(Math.random() * 27) + 1) * 10
+        // 赋给食物 给与刷新
         this.element.style.left = left + 'px';
         this.element.style.top = top + 'px';
     }
--- src/moduls/ScorePanel.ts
+++ src/moduls/ScorePanel.ts
@@ -6,9 +6,9 @@ class ScorePanel {
     scoreEle: HTMLElement;
     levelEle: HTMLElement;
 
-    maxlevel: number;
-    Upscore: number;
-    constructor(maxlevel = 10, upScore = 3) {
+    maxlevel: number;   //最大等级
+    Upscore: number; // 每级经验数
+    constructor(maxlevel = 10, upScore = 2) {
         this.scoreEle = document.getElementById('score')!;
         this.levelEle = document.getElementById('level')!;
         this.maxlevel = maxlevel
@@ -18,7 +18,7 @@ class ScorePanel {
     //加分方法
     addScore() {
         this.score++;
-        // span 标签需要一个字符串来表示 
+        // span 标签需要一个字符串来表示  隐形转化成字符串
         this.scoreEle.innerHTML = this.score + '';
         // 每多少分升级
         if (this.score % this.Upscore === 0) {
@@ -31,8 +31,6 @@ class ScorePanel {
             this.level++
             this.levelEle.innerHTML = this.level + '';
         }
-
-
     }
 
 }
